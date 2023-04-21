2850be4b8e47e17a4ead8280c5d30cb221db333f
Cristhian Melo <rxtsel@hotmail.com>
2023-04-21 14:10:25 -0500

    feat(dep): add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), tailwind()],
   base: "/",
-  site: "https://quantto.io",
+  site: "https://quantto.io"
 });
