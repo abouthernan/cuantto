4d9f3e84d3dbfe766761e32a4f6ef46b2f6666c1
Cristhian Melo <rxtsel@hotmail.com>
2023-04-25 17:24:14 -0500

    feat: add config to github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  base: '/',
+  site: 'https://qantto.io'
 });
