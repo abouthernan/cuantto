8338fa5f8aa2ad5f6ca0f5d6e9b28ab2773157ec
Cristhian Melo <rxtsel@hotmail.com>
2023-04-25 17:32:49 -0500

    fix: change class to className on hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ export const Hero = () => {
       className="w-screen z-10 flex justify-center items-center bg-customDarkBg1 mb-[28vw] md:mb-[18vw] lg:mb-[10vw] xl:mb-[13vw] 2xl:mb-60 hero-bg-gradient pb-24 sm:pb-32 md:pb-44 lg:pb-0 relative"
       id="home"
     >
-      <span class="ondas" />
+      <span className="ondas" />
       <div className="w-full md:w-[800px] xl:w-[900px] flex flex-col justify-center items-center pt-16 md:pt-16 lg:pt-20 text-center">
         <motion.div
           initial={{ opacity: 0, y: 10 }}
