201e0e7730a85b989f2d33a85ef10ed9f87abfcf
Cristhian Melo <rxtsel@hotmail.com>
2023-05-24 13:03:28 -0500

    feat: add counter clients

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+dotenv.config();
+
+const uri = process.env.MONGO_URL;
+
+export const client = new MongoClient(uri);
+const databaseName = 'clients';
+const collectionName = 'counts';
+
+export const extractCount = async () => {
+  try {
+    await client.connect();
+    const database = client.db(databaseName);
+    const counts = database.collection(collectionName);
+
+    const countDocument = await counts.findOne({});
+    return countDocument.count;
+  } finally {
+    // No cerramos la conexión aquí
+  }
+};
+
+export const incrementCount = async (newCount: number) => {
+  try {
+    await client.connect();
+    const database = client.db(databaseName);
+    const counts = database.collection(collectionName);
+
+    await counts.updateOne({}, { $set: { count: newCount } });
+  } finally {
+    // No cerramos la conexión aquí
+  }
+};
---
+++ src/pages/api/clients.js
@@ -0,0 +1,33 @@
+import { extractCount, incrementCount } from '../../lib/mongodb.ts'
+
+export async function get() {
+  const count = await extractCount()
+
+  return new Response(JSON.stringify({ count }), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+}
+
+export async function post({ request }) {
+  let { count } = await request.json()
+  const currentCount = await extractCount()
+  const newCount = currentCount + 1
+
+  if (count !== currentCount) {
+    // El contador en la solicitud POST no coincide con el contador actual
+    // Puedes manejar esto según tus requisitos, ya sea lanzando un error o actualizando el contador en consecuencia
+    count = currentCount
+  }
+
+  await incrementCount(newCount)
+
+  return new Response(null, {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+}
