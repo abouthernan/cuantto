b27c036b0c834968050b85a6dc1643bed630132b
Cristhian Melo <rxtsel@hotmail.com>
2023-05-29 15:14:02 -0500

    feat: add support to multilanguage

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/serverless'
+import astroI18next from 'astro-i18next'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), astroI18next()],
   base: '/',
   site: 'https://qantto.io',
   output: 'server',
