c595e2e1aeaeb457aa74045cdfc173ac4d80f77d
Cristhian Melo <rxtsel@hotmail.com>
2023-06-07 10:21:48 -0500

    chore: resize textimonials box

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -6,7 +6,7 @@ import testimonial2 from '../assets/images/testimonial2.png'
 import testimonial3 from '../assets/images/testimonial3.png'
 import { useTranslation } from 'react-i18next'
 
-export const Testimonials = () => { 
+export const Testimonials = () => {
   const { t } = useTranslation()
 
   const testimonialsData = [
@@ -32,7 +32,7 @@ export const Testimonials = () => {
 
   return (
     <section className="w-full flex justify-center pt-10 mb-16 lg:mb-32 bg-customBg relative">
-      <div className="absolute -top-16" id="feedback" />
+      // <div className="absolute -top-16" id="feedback" />
       <div className="flex flex-col w-full lg:w-[1150px] justify-center">
         <motion.div
           initial={{ opacity: 0 }}
@@ -50,7 +50,7 @@ export const Testimonials = () => {
           <div className="flex flex-col lg:flex-row gap-8 lg:gap-5 xl:gap-10 px-6 xl:px-0 items-center">
             {testimonialsData.map((testimonial, index) => (
               <div
-                className="w-11/12 sm:w-4/5 md:w-[560px] lg:w-1/3 custom-border-gray-darker rounded-xl bg-customDarkBg3 flex flex-col px-6 py-4"
+                className="w-11/12 relative h-[480px] sm:w-4/5 md:w-[560px] lg:w-1/3 custom-border-gray-darker rounded-xl bg-customDarkBg3 flex flex-col px-6 py-4"
                 key={`${testimonial.customerName}-${index}`}
               >
                 <div className="flex mb-2">
@@ -59,7 +59,7 @@ export const Testimonials = () => {
                 <div className="custom-content-text-white">
                   "{testimonial.content}"
                 </div>
-                <div className="flex mt-4 mb-2 xl:mt-8 xl:mb-4">
+                <div className="flex mt-4 mb-2 xl:mt-8 xl:mb-4 absolute bottom-2">
                   <img src={testimonial.image} alt="" width="45px" />
                   <div className="flex flex-col ml-4">
                     <div className="custom-content-text-white font-medium">
@@ -76,4 +76,5 @@ export const Testimonials = () => {
         </motion.div>
       </div>
     </section>
-)}
+  )
+}
