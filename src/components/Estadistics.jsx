01c25b749cb9b8622df6a045a84514288a53e817
Cristhian Melo <rxtsel@hotmail.com>
2023-06-21 11:48:08 -0500

    chore: add porcentaje color green

--- src/components/Estadistics.jsx
+++ src/components/Estadistics.jsx
@@ -2,15 +2,38 @@ import { useTranslation } from 'react-i18next'
 
 export function Estadistics() {
   const { t } = useTranslation()
+
   const ESTADISTICS = [
-    t('ESTADISTICS.one'),
-    t('ESTADISTICS.two'),
-    t('ESTADISTICS.three'),
-    t('ESTADISTICS.four'),
-    t('ESTADISTICS.five'),
-    t('ESTADISTICS.six'),
-    t('ESTADISTICS.seven')
+    {
+      text: t('ESTADISTICS.one'),
+      value: '37%'
+    },
+    {
+      text: t('ESTADISTICS.two'),
+      value: '41%'
+    },
+    {
+      text: t('ESTADISTICS.three'),
+      value: '90%'
+    },
+    {
+      text: t('ESTADISTICS.four'),
+      value: '60%'
+    },
+    {
+      text: t('ESTADISTICS.five'),
+      value: '66%'
+    },
+    {
+      text: t('ESTADISTICS.six'),
+      value: '95%'
+    },
+    {
+      text: t('ESTADISTICS.seven'),
+      value: '64%'
+    }
   ]
+
   return (
     <div
       className="container block mx-auto mb-28 -my-14 overflow-hidden pt-4 2xl:w-[1280px] xl:w-10/12 w-11/12 mt-10"
@@ -26,9 +49,13 @@ export function Estadistics() {
               <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-customGreen opacity-75" />
               <span className="relative inline-flex rounded-full h-2 w-2 bg-customGreen" />
             </span>
-            {stadistic}
+            <span className="text-customGreen text-2xl font-semibold">
+              {stadistic.value}
+            </span>
+            {stadistic.text}
           </li>
         ))}
+
         {ESTADISTICS.map((stadistic, index) => (
           <li
             key={index}
@@ -38,7 +65,10 @@ export function Estadistics() {
               <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-customGreen opacity-75" />
               <span className="relative inline-flex rounded-full h-2 w-2 bg-customGreen" />
             </span>
-            {stadistic}
+            <span className="text-customGreen text-2xl font-semibold">
+              {stadistic.value}
+            </span>
+            {stadistic.text}
           </li>
         ))}
       </ul>
