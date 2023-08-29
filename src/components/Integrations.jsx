ad717c951a51f6c7c26da0732bba8a64902e6999
Cristhian Melo <rxtsel@hotmail.com>
2023-08-29 11:22:34 -0500

    fix: change class with className in react component

--- src/components/Integrations.jsx
+++ src/components/Integrations.jsx
@@ -17,66 +17,59 @@ export const Integrations = () => {
   const { t } = useTranslation()
   return (
     <div
-      class="container block mx-auto mb-24 -my-14 overflow-hidden pt-4 2xl:w-[1280px] xl:w-10/12 w-11/12 mt-10"
+      className="container block mx-auto mb-24 -my-14 overflow-hidden pt-4 2xl:w-[1280px] xl:w-10/12 w-11/12 mt-10"
       id="services"
     >
+      <span className="custom-block-subtitle text-center block">
+        {t('INTEGRATIONS.sub')}
+      </span>
+      <h2 className="text-white text-center text-4xl lg:text-5xl font-bold my-6">
+        {t('INTEGRATIONS.title')}
+      </h2>
+      <p className="text-gray-300 w-full text-center mb-10">
+        {t('INTEGRATIONS.description')}
+      </p>
 
-      <span class="custom-block-subtitle text-center block">{t("INTEGRATIONS.sub")}</span>
-      <h2 class="text-white text-center text-4xl lg:text-5xl font-bold my-6">{t("INTEGRATIONS.title")}</h2>
-      <p class="text-gray-300 w-full text-center mb-10">{t("INTEGRATIONS.description")}</p>
-
-      <ul
-        class="gap-x-5 animate-integrations overflow-hidden space-x-4 sm:space-x-10 inline-flex w-[calc(240px*10)]"
-      >
-        {
-          integrations1.map(item => (
-            <li class="relative h-28 w-60 rounded-md text-gray-300">
-              <img
-                src={item}
-                alt="logo"
-                class="aspect-video object-contain object-center w-full h-full"
-              />
-            </li>
-          ))
-        }
-        {
-          integrations1.map(item => (
-            <li class="relative h-28 w-60 rounded-md text-gray-300">
-              <img
-                src={item}
-                alt="logo"
-                class="aspect-video object-contain object-center w-full h-full"
-              />
-            </li>
-          ))
-        }
+      <ul className="gap-x-5 animate-integrations overflow-hidden space-x-4 sm:space-x-10 inline-flex w-[calc(240px*10)]">
+        {integrations1.map(item => (
+          <li className="relative h-28 w-60 rounded-md text-gray-300">
+            <img
+              src={item}
+              alt="logo"
+              className="aspect-video object-contain object-center w-full h-full"
+            />
+          </li>
+        ))}
+        {integrations1.map(item => (
+          <li className="relative h-28 w-60 rounded-md text-gray-300">
+            <img
+              src={item}
+              alt="logo"
+              className="aspect-video object-contain object-center w-full h-full"
+            />
+          </li>
+        ))}
       </ul>
 
-      <ul
-        class="gap-x-5 animate-integrations overflow-hidden space-x-4 sm:space-x-10 inline-flex w-[calc(240px*10)] mt-5 direction-reverse"
-      >
-        {
-          integrations2.map(item => (
-            <li class="relative h-28 w-60 rounded-md text-gray-300">
-              <img
-                src={item}
-                alt="logo"
-                class="aspect-video object-contain object-center w-full h-full"
-              />
-            </li>
-          ))
-        }
-        {
-          integrations2.map(item => (
-            <li class="relative h-28 w-60 rounded-md text-gray-300">
-              <img
-                src={item}
-                alt="logo"
-                class="aspect-video object-contain object-center w-full h-full"
-              />
-            </li>
-          ))
-        }
+      <ul className="gap-x-5 animate-integrations overflow-hidden space-x-4 sm:space-x-10 inline-flex w-[calc(240px*10)] mt-5 direction-reverse">
+        {integrations2.map(item => (
+          <li className="relative h-28 w-60 rounded-md text-gray-300">
+            <img
+              src={item}
+              alt="logo"
+              className="aspect-video object-contain object-center w-full h-full"
+            />
+          </li>
+        ))}
+        {integrations2.map(item => (
+          <li className="relative h-28 w-60 rounded-md text-gray-300">
+            <img
+              src={item}
+              alt="logo"
+              className="aspect-video object-contain object-center w-full h-full"
+            />
+          </li>
+        ))}
       </ul>
     </div>
   )
