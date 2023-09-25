2f4863c8edfc4196a9f96c497a5c16d7b33f0fd4
Cristhian Melo <rxtsel@hotmail.com>
2023-09-25 11:52:01 -0500

    chore: add constants

---
+++ src/constants/menu.ts
@@ -0,0 +1,29 @@
+export const menu = [
+  {
+    label: 'Beneficios',
+    path: '/#beneficios',
+  },
+  {
+    label: 'Planes',
+    path: '/#planes',
+  },
+  {
+    label: 'Recursos',
+    path: '/#recursos',
+  },
+  {
+    label: 'Preguntas',
+    path: '/#preguntas',
+  }
+];
+
+export const menuButtons = [
+  {
+    label: 'Ingresar',
+    path: '#',
+  },
+  {
+    label: 'Inicia tu Free Trial',
+    path: '#',
+  },
+];
