3a486503d9d1a3a4033400abfa423a9e9b92eb9d
Cristhian Melo <rxtsel@hotmail.com>
2023-09-25 11:18:56 -0500

    chore(dev): add custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			qGreen: '#6EF96E',
+			qBg: '#1F2122',
+			qBlack: '#1E1E1E',
+			qGray: '#303037',
+			qPurple: '#E66EF9',
+			qSky: '#6ED8F9',
+			qRed: '#FF0B0B',
+			qPaperGreen: 'rgba(110, 249, 110, 0.1)',
+		},
 		extend: {},
 	},
 	plugins: [],
