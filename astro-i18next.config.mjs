e1b01625aac71f5ac5bc9d8959e6d4778ab2b12f
Cristhian Melo <rxtsel@hotmail.com>
2023-10-12 16:59:17 -0500

    chore: update config

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -3,6 +3,12 @@ export default {
   defaultLocale: "es",
   locales: ["es", "en"],
   load: ["server", "client"],
+  i18nextServer: {
+    debug: true,
+  },
+  i18nextClient: {
+    debug: true,
+  },
   i18nextClientPlugins: {
     "{initReactI18next}": "react-i18next",
   },
