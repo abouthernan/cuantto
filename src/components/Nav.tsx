30a2ec0f44c8930b27ef066e7cd617fe7f269685
Cristhian Melo <rxtsel@hotmail.com>
2023-10-26 12:33:24 -0500

    test: test suspense i18n

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 import { dashboardRouter } from "../constants";
 
 export const Nav = () => {
-  const { t } = useTranslation("translation");
+  const { t, ready } = useTranslation("translation");
 
   const menu = [
     {
@@ -35,6 +35,8 @@ export const Nav = () => {
     },
   ];
 
+  if (!ready) return null;
+
   return (
     <>
       <nav className="grid nav-mobil duration-300 lg:hidden fixed px-2 w-full top-0 left-0 py-5 backdrop-blur-xl right-0 bottom-0 place-items-center h-[calc(100vh-88px)] text-center md:space-x-4 items-center transition-all -translate-y-full bg-qBg">
@@ -42,6 +44,7 @@ export const Nav = () => {
           <a
             className="text-white/90 font-normal text-lg py-3 w-full"
             href={path}
+            key={label}
           >
             {label}
           </a>
@@ -52,6 +55,7 @@ export const Nav = () => {
             <a
               className="text-white text-center font-normal text-lg rounded-md py-2 px-4 border border-transparent transition-colors duration-300 ease-in-out"
               href={path}
+              key={label}
             >
               {label}
             </a>
@@ -63,7 +67,7 @@ export const Nav = () => {
 
       <nav className="hidden lg:flex place-items-center text-center space-x-10 items-center">
         {menu.map(({ label, path }) => (
-          <a className="text-white/90 font-normal" href={path}>
+          <a className="text-white/90 font-normal" href={path} key={label}>
             {label}
           </a>
         ))}
@@ -73,6 +77,7 @@ export const Nav = () => {
             <a
               className="text-white text-center font-bold rounded-md py-2 border border-transparent transition-colors duration-300 ease-in-out"
               href={path}
+              key={label}
             >
               {label}
             </a>
